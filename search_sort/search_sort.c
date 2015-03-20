325edee5a0ad149b6818d4272d8e1e235065ee86
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-03-21 00:25:50 +0300

    Initial commit<search_sort.c>

---
+++ search_sort/search_sort.c
@@ -0,0 +1,15 @@
+/*
+Search and sort algorithms
+@__author Denis Karanja
+@__date 21st March, 2015 00:18:30
+*/
+#include <stdio.h>
+#include "headers/search_sort_header.h"
+
+int main()
+{
+	int numbers[] = {20, 10, 15, -4, 5, 22, 34, 65, -3, 34};
+	int i;
+	/*maximum value in an array*/
+
+}
